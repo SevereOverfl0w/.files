5a1e5aac543ea18b4655ab6b25ad5bc6193b3d7d
SevereOverfl0w <overfl0w@narna.co>
2015-05-19 15:24:08 +0100

    label those workspaces!

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -5,6 +5,9 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.WorkspaceNames
+import XMonad.Prompt
+
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -13,7 +16,7 @@ main = do
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-        , logHook = dynamicLogWithPP xmobarPP
+        , logHook = dynamicLogWithPP =<< workspaceNamesPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
@@ -22,6 +25,9 @@ main = do
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
+        , ((mod4Mask .|. shiftMask, xK_r      ), renameWorkspace defaultXPConfig)
+        , ((mod4Mask .|. shiftMask, xK_Left  ), swapTo Prev)
+        , ((mod4Mask .|. shiftMask, xK_Right ), swapTo Next)
+          , ((0, xK_Print), spawn "scrot")
         ]
 
