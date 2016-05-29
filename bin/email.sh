8ed0894d6788e23e7da213ce336b09c15a12fc32
Dominic <dominic@juxt.pro>
2016-05-29 23:58:45 +0100

    Combine sync/sort into one script

---
+++ bin/email.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Organize the emails
+/usr/bin/imapfilter -c ~/.config/imapfilter/work.lua
+# Pull email down, if imapfilter succeeded
+if [ $? -eq 0 ]; then
+  /usr/bin/mbsync -Va
+fi
