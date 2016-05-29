11f441d4bc8eea3f98c314c7a83c03cdb01eae46
Dominic <dominic@juxt.pro>
2016-05-29 23:56:01 +0100

    Thicken lemonbar underline

--- bin/panel/lemonbar.sh
+++ bin/panel/lemonbar.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/panel_vars.sh
 
 bspc config top_padding $PANEL_HEIGHT
-$DIR/reload.sh | lemonbar -p -f TamzenForPowerline-17 -f FontAwesome-12 -B $dark0_soft -u 3 -U "#f00" | sh
+$DIR/reload.sh | lemonbar -p -f TamzenForPowerline-17 -f FontAwesome-12 -B $dark0_soft -u 2 -U "#f00" | sh
