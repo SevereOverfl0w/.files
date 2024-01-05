f0b39f15c19411660979716b0d1b88949c497530
Dominic Monroe <dominic@dominic.io>
2024-01-05 09:42:39 +0000

    Add missing `saved-value` function
    Workaround for https://github.com/vvvvalvalval/scope-capture/issues/50

--- clojure/dev.clj
+++ clojure/dev.clj
@@ -2,12 +2,20 @@
 
 (require '[com.gfredericks.dot-slash-2 :as dot-slash-2])
 
+
+(defn saved-value
+  "Return either value or exception of ep-info"
+  [& args]
+  (some (apply (requiring-resolve 'sc.api/ep-info) args)
+        [:sc.ep/value :sc.ep/error]))
+
 (dot-slash-2/!
   '{. [clojure.test/run-tests clojure.test/test-vars
        {:var sc.api/spy :lazy? true :macro? true}
        {:var sc.api/letsc :lazy? true :macro? true}
        {:var sc.api/defsc :lazy? true :macro? true}
        {:var sc.api/undefsc :lazy? true :macro? true}
+       {:var io.dominic.mise/saved-value}
        {:var clj-async-profiler.core/profile :lazy? true :macro? true}
        {:var clj-async-profiler.core/serve-ui :lazy? true :macro? false}
        {:var clj-memory-meter.core/measure :lazy? true :macro? false}
