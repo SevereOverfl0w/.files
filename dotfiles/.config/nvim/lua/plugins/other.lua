0082c9002b3ee949583a27673acb2cf9191b2a92
Dominic Monroe <dominic@dominic.io>
2025-04-24 08:06:48 +0100

    Add other.lua case for cljc impl->clj test

--- dotfiles/.config/nvim/lua/plugins/other.lua
+++ dotfiles/.config/nvim/lua/plugins/other.lua
@@ -4,6 +4,7 @@ return {
     main = "other-nvim",
     opts = {
       mappings = {
+        -- src/foo.cljc -> test/foo_test.cljc
         {
           context = "test",
           pattern = function(path)
@@ -14,6 +15,18 @@ return {
           end,
           target = "%2/test/%4_test.clj%5"
         },
+        -- src/foo.cljc -> test/foo_test.clj
+        {
+          context = "test",
+          pattern = function(path)
+            local match = vim.fn.matchlist(path, '\\v^(.*)/src/(.{-}_test)@!(.{-}).cljc')
+            if #match > 0 then
+              return match
+            end
+          end,
+          target = "%2/test/%4_test.clj"
+        },
+        -- test/foo_test.cljc -> src/foo.cljc
         {
           context = "implementation",
           pattern = "(.*)/test/(.*)_test.clj(.?)$",
