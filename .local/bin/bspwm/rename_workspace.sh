e3c904b0c5e02c33c798009b34a3f0e9413bfcb8
Dominic Monroe <dominic@dominic.io>
2026-07-11 19:07:55 +0100

    Remove X
    Long live Wayland

--- .local/bin/bspwm/rename_workspace.sh
+++
@@ -1,5 +0,0 @@
-NAME=$(rofi -dmenu -p "New name: " < ~/.files/workspaces.txt)
-
-if [ -n "$NAME" ]; then
-  bspc desktop -n "$NAME"
-fi
--- .local/bin/i3/rename_workspace.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-NAME=$(echo "" | rofi -dmenu -p "New name: ")
-~/.files/bin/i3/rename_workspace "$NAME"
